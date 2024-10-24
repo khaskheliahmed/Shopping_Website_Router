63576deb76cf297cfa3512176fc7e8ede97c5b04
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2024-10-25 01:02:17 +0500

    change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Card from './compounts/card'
-import Header from './compounts/header'
+
 
 const Layout = () => {
   return (
